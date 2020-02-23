f8bc5a2669b6febc6a5af5a440d1391751178270
Manoj S <manojs_aws@yahoo.com>
2020-02-24 02:48:22 +0530

    Spring (Boot) Eureka with Docker Containerization

---
+++ src/test/java/com/manojs/springeureka/EurekaServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.manojs.springeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
